bab2f895db0bfc707a4c75fc098b8289772f96d8
matchachan <61008981+matchachan@users.noreply.github.com>
2020-02-26 15:48:04 +0900

    change

--- 2-c.go
+++ 2-c.go
@@ -2,10 +2,10 @@ package main
 import "fmt"
 
 func main(){
-  var bubble [3]int
-  for i:=0;i<3;i++{
+  bubble :=[3]int{1,2,3}
+  /*for i:=0;i<3;i++{
     fmt.Scanf("%d",&bubble[i])
-  }
+  }*/
   for i:=0;i<3;i++{
     fmt.Printf("%d\n",bubble[i])
   }
